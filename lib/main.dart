f82b881f5b1cc9c93bc050bbfc63061b701a110c
Aziz Riza <saitamasensei710@gmail.com>
2022-04-02 16:06:47 +0700

    kedua

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,30 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.blueGrey[300],
+          leading: Icon(Icons.home),
           title: Text("Belajar Dasar"),
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: Icon(Icons.search, color: Colors.red)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.call))
+          ],
         ),
-        body: Center(
-          child: Text("Awal"),
+        body: Container(
+          decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2022/02/12/2805017866.jpg'),
+                fit: BoxFit.cover,
+              ),
+              border: Border.all(
+                color: Colors.black,
+                width: 10,
+              ),
+              borderRadius: BorderRadius.circular(20)),
+          height: 200,
+          margin: EdgeInsets.only(top: 20),
         ));
   }
 }
